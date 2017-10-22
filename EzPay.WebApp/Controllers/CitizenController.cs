1b9e8562311803196fc98a2bb2fc14381ed1bee9
George Antonopoulos <georgeantonopoulosga@hotmail.com>
2017-10-22 11:32:43 +0300

    basic static files and partial views

---
+++ EzPay.WebApp/Controllers/CitizenController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EzPay.WebApp.Controllers
+{
+    public class CitizenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Login Get
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        //Login Post
+
+        //Logout
+    }
+}
--- EzPay.WebApp/Startup.cs
+++ EzPay.WebApp/Startup.cs
@@ -9,12 +9,22 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace EzPay.WebApp
 {
+    using Microsoft.Extensions.Configuration;
+
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +34,19 @@ namespace EzPay.WebApp
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+                {
+                    routes.MapRoute(
+                        name: "default",
+                        template: "{controller=Home}/{action=Index}/{id?}");
+                });
         }
     }
 }
