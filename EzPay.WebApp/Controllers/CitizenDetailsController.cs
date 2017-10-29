c035a6ee033ec3947e45122389eaf5695e48a16c
George Kolovos <kolovos.giwrgos@gmail.com>
2017-10-29 17:06:56 +0200

    Adding ControllerDetails

---
+++ EzPay.WebApp/Controllers/CitizenDetailsController.cs
@@ -0,0 +1,37 @@
+﻿using EzPay.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EzPay.WebApp.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class CitizenDetailsController : Controller
+    {
+        private readonly EzPayContext _context;
+
+        public CitizenDetailsController(EzPayContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Citizen/Details/5
+        [HttpGet("{id:long}")]
+        public async Task<IActionResult> Details(long id)
+        {
+            var model = await _context.Citizens.Where(c => c.Id == id)
+                .FirstOrDefaultAsync();
+
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return View(model);
+        }
+    }
+}
