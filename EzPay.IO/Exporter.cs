93f2b1ad022568ac1c1c39763e6d65ee71320d7d
Alexandros Kritikos <akritikos@users.noreply.github.com>
2017-10-30 21:19:36 +0200

    Exporter updated & usage example on ModelUpdater.Program

--- EzPay.IO/Exporter.cs
+++ EzPay.IO/Exporter.cs
@@ -19,9 +19,24 @@ namespace EzPay.IO
     public class Exporter
     {
         /// <summary>
+        /// Header for Settlement export file
+        /// </summary>
+        private const string SettlementHeader = "VAT; TIME;BILLS;DOWNPAYMENT;INSTALLMENTS;INTEREST";
+
+        /// <summary>
+        /// Header for Payment export file
+        /// </summary>
+        private const string PaymentHeader = "BILL_ID;TIME;AMOUNT;METHOD";
+
+        /// <summary>
         /// FileHelperEngine managing <see cref="Payment"/> records
         /// </summary>
         private readonly FileHelperEngine<PaymentExport> paymentEngine;
+
+        /// <summary>
+        /// FileHelperEngine managing <see cref="Settlement"/> records
+        /// </summary>
+        private readonly FileHelperEngine<SettlementExport> settlementEngine;
 
         /// <summary>
         /// List containing <see cref="Payment"/> records to be exported
@@ -29,18 +44,35 @@ namespace EzPay.IO
         private readonly List<PaymentExport> paymentExports;
 
         /// <summary>
+        /// List containing <see cref="Settlement"/> records to be exported
+        /// </summary>
+        private readonly List<SettlementExport> settlementExports;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="Exporter"/> class.
         /// </summary>
         public Exporter()
         {
             paymentExports = new List<PaymentExport>();
-            paymentEngine = new FileHelperEngine<PaymentExport>();
+            settlementExports = new List<SettlementExport>();
+
+            paymentEngine = new FileHelperEngine<PaymentExport>() { HeaderText = PaymentHeader };
+            settlementEngine = new FileHelperEngine<SettlementExport>() { HeaderText = SettlementHeader };
             using (var ctx = new EzPayContext())
             {
                 var payments = ctx.Payments.Include(c => c.Bill).ToList();
+                var settlements = ctx.Settlements
+                    .Include(s => s.Bills)
+                    .Include(s => s.Type)
+                    .Include(s => s.Citizen)
+                    .ToList();
                 foreach (var payment in payments)
                 {
                     paymentExports.Add(new PaymentExport(payment));
+                }
+                foreach (var settlement in settlements)
+                {
+                    settlementExports.Add(new SettlementExport(settlement));
                 }
             }
         }
@@ -55,6 +87,17 @@ namespace EzPay.IO
             {
                 StreamWriter write = new StreamWriter(fi);
                 paymentEngine.WriteStream(write, paymentExports);
+                write.Flush();
+                write.Close();
+            }
+        }
+
+        public void ExportSettlements(string path)
+        {
+            using (var fi = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                var write = new StreamWriter(fi);
+                settlementEngine.WriteStream(write, settlementExports);
                 write.Flush();
                 write.Close();
             }
