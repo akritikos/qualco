5b2227562b2a790737e0a278d64e85af49080ab9
Alexandros Kritikos <akritikos@users.noreply.github.com>
2017-11-05 11:53:10 +0200

    Creates a BillComparer that sorts by Due Date

---
+++ EzPay.Model/Comparer/BillCompareByDate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EzPay.Model.Comparer
+{
+    using EzPay.Model.Entities;
+
+    /// <inheritdoc cref="IComparer{T}" />
+    /// <inheritdoc cref="IEqualityComparer{T}" />
+    public class BillCompareByDate : IComparer<Bill>, IEqualityComparer<Bill>
+    {
+        /// <inheritdoc />
+        public bool Equals(Bill x, Bill y)
+        {
+            return x.DueDate.Equals(y.DueDate);
+        }
+
+        /// <inheritdoc />
+        public int GetHashCode(Bill obj)
+        {
+            return obj.DueDate.GetHashCode();
+        }
+
+        /// <inheritdoc />
+        public int Compare(Bill x, Bill y)
+        {
+            return x.DueDate.CompareTo(y.DueDate);
+        }
+    }
+}
