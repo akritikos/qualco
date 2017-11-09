416e1d920449bb337c8df46e89e31ab8fb858e32
Alexandros Kritikos <akritikos@users.noreply.github.com>
2017-11-09 18:50:28 +0200

    Defines IQualcoRepository to abstract the actual DbContext

---
+++ EzPay.Model/IQualcoRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace EzPay.Model
+{
+    using EzPay.Model.Entities;
+    using EzPay.Model.IdentityEntities;
+    using EzPay.Model.Interfaces;
+
+    using Microsoft.EntityFrameworkCore;
+
+    /// <summary>
+    /// Provides a source-agnostic EzPay model 
+    /// </summary>
+    public interface IQualcoRepository
+    {
+        /// <summary>
+        /// Clears volatile data from the database: <see cref="Payment"/>, <see cref="Bill"/>, <see cref="Settlement"/>
+        /// </summary>
+        void ClearVolatile();
+
+        DbSet<T> GetSet<T>() where T : class, IEntity;
+    }
+}
