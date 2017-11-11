0e7e9bc677cc427becf082bad429ed7a6408b67d
George Kolovos <kolovos.giwrgos@gmail.com>
2017-11-11 15:55:54 +0200

    Add about page sample

--- EzPay.WebApp/Models/LoginViewModel.cs
+++ EzPay.WebApp/Models/LoginViewModel.cs
@@ -9,7 +9,7 @@ namespace EzPay.WebApp.Models
     public class LoginViewModel
     {
         [Required(ErrorMessage = "Your Citizen ID is required.")]
-        [Display(Name ="Citizen ID")]
+        [Display(Name = "Citizen ID")]
         public long CitizenId { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
@@ -20,11 +20,30 @@ namespace EzPay.WebApp.Models
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Address { get; set; }
         public string County { get; set; }
         public string Email { get; set; }
-        public string Address { get; set; }
         public string PhoneNumber { get; set; }
 
         public IEnumerable<Bill> Bills { get; set; }
+        public IEnumerable<Settlement> Settlements { get; set; }
+
+
+        /*change password*/
+
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string StatusMessage { get; set; }
+
+        /*...change password*/
+
     }
 }
