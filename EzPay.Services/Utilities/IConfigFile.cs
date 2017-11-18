8bd1e0d4921584c24661bfd4967562c7c64e6cf0
Alexandros Kritikos <akritikos@users.noreply.github.com>
2017-11-18 04:47:35 +0200

    Adds a console app exporting Payments & Settlements

---
+++ EzPay.Services/Utilities/IConfigFile.cs
@@ -0,0 +1,14 @@
+﻿namespace EzPay.Services.Utilities
+{
+    using System.IO;
+
+    /// <summary>
+    /// Generic file storage for config settings
+    /// </summary>
+    public interface IConfigFile
+    {
+        string GetConfigValue(string key);
+
+        void LoadConfig(FileInfo file);
+    }
+}
