4aa06266eb4bdbea6f14fcb5cdc122b0d7e9f5e7
Donna <serbeti@hotmail.com>
2017-11-18 18:39:23 +0200

    Contact us form

--- EzPay.WebApp/Controllers/HomeController.cs
+++ EzPay.WebApp/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using EzPay.WebApp.Models;
 using System.Diagnostics;
+using EzPay.EmailSender;
 
 namespace EzPay.WebApp.Controllers
 {
@@ -31,6 +32,18 @@ namespace EzPay.WebApp.Controllers
             ViewBag.Message = "Contact page for the portal.";
 
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendForm(ContactViewModel model)
+        {
+            var sender = new SmtpSender("EZPayVerify", "1q2w3e4r5t6y!@#", "smtp.gmail.com", ssl: true);
+            string mailbody = $"Subject={model.Subject}<BR>UserId={model.UserId}<BR>Email={model.Email}<BR>Message={model.Message}";
+            sender.SetParameters("EZPayVerify@gmail.com", "EZPayVerify@gmail.com", "ezpay", $"Contact Form Input {DateTime.Now}", string.Empty, mailbody);
+            await sender.Send();
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+
         }
 
         public IActionResult Error() => 
