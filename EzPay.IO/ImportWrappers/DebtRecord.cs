e83695c0590225886b40774b2d1fda30a4d3ef63
Alexandros Kritikos <akritikos@users.noreply.github.com>
2017-11-18 04:04:44 +0200

    Propagates changes from 798888a942b51fe4d63b424752ac612683e32e91 to import wrappers

---
+++ EzPay.IO/ImportWrappers/DebtRecord.cs
@@ -0,0 +1,98 @@
+﻿#pragma warning disable 649
+
+namespace EzPay.IO.ImportWrappers
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+
+    using EzPay.IO.Interfaces;
+    using EzPay.Model.Entities;
+
+    using FileHelpers;
+
+    /// <summary>
+    /// Wrapper class for parsing CSV records
+    /// </summary>
+    [IgnoreFirst, DelimitedRecord(";")]
+    [SuppressMessage("ReSharper", "StyleCop.SA1600", Justification = "Fields are documented in respective classes")]
+    public class DebtRecord : IEntityRecord
+    {
+        [FieldHidden]
+        private Citizen c;
+
+        [FieldHidden]
+        private Bill b;
+
+        #region Citizen
+        [FieldNotEmpty]
+        private long citizenVat;
+
+        [FieldNotEmpty]
+        private string citizenName;
+
+        [FieldNotEmpty]
+        private string citizenSurname;
+
+        [FieldNotEmpty]
+        private string citizenEmail;
+
+        [FieldNotEmpty]
+        private int citizenPhone;
+
+        [FieldNotEmpty]
+        private string citizenAddress;
+
+        [FieldNotEmpty]
+        private string citizenCounty;
+        #endregion Citizen
+
+        #region Bill
+        [FieldNotEmpty]
+        private string billId;
+
+        [FieldNotEmpty]
+        private string billDescription;
+
+        [FieldNotEmpty]
+        private string billAmount;
+
+        [FieldNotEmpty]
+        private string billDate;
+        #endregion
+
+        /// <summary>
+        /// Parses data from this record into a new <see cref="Citizen"/> object
+        /// </summary>
+        /// <returns>Valid <see cref="Citizen"/> object</returns>
+        public Citizen ParseCitizen() => this.c = this.c ?? new Citizen
+                                                      {
+                                                          Id = this.citizenVat,
+                                                          FirstName = this.citizenName,
+                                                          LastName = this.citizenSurname,
+                                                          Email = this.citizenEmail,
+                                                          PhoneNumber = this.citizenPhone.ToString(),
+                                                          Address = this.citizenAddress,
+                                                          County = this.citizenCounty
+                                                      };
+
+        /// <summary>
+        /// Parses data from this record into a new <see cref="Bill"/> object
+        /// </summary>
+        /// <returns>Valid <see cref="Bill"/> object</returns>
+        public Bill ParseBill() => this.b = this.b ?? new Bill
+                                                {
+                                                    Id = Guid.Parse(this.billId.ToUpper()),
+                                                    CitizenId = this.citizenVat,
+                                                    Amount = decimal.Parse(
+                                                        this.billAmount.Replace(",", "."),
+                                                        NumberStyles.Float,
+                                                        CultureInfo.InvariantCulture),
+                                                    Description = this.billDescription,
+                                                    DueDate = DateTime.ParseExact(
+                                                        this.billDate,
+                                                        "yyyyMMdd",
+                                                        CultureInfo.InvariantCulture)
+                                                };
+    }
+}
