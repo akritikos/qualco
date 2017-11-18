ed1d1c5135e36c7df597ee4fcc017ff8f1913e28
Alexandros Kritikos <akritikos@users.noreply.github.com>
2017-11-18 03:34:40 +0200

    Moves Export wrappers to their own namespace and reworks fields to perform better

---
+++ EzPay.IO/ExportWrappers/PaymentExport.cs
@@ -0,0 +1,47 @@
+﻿namespace EzPay.IO.ExportWrapper
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    using EzPay.IO.Interfaces;
+    using EzPay.Model.Entities;
+
+    using FileHelpers;
+
+    /// <summary>
+    /// Wrapper class for exporting CSV records of <see cref="Payment"/> entities
+    /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Required formatting for export, avoiding data annotation.")]
+    [SuppressMessage("ReSharper", "StyleCop.SA1600", Justification = "Properties are private & documented in Payment class")]
+    [SuppressMessage("ReSharper", "StyleCop.SA1401", Justification = "Entity is meant for record parsing, access modifier is Public for FileHelpers interop on fields")]
+    [DelimitedRecord(";")]
+    public class PaymentExport : IEntityRecord
+    {
+        public string BILL_ID;
+
+        public string TIME;
+
+        [FieldConverter(ConverterKind.Decimal, ",")]
+        public decimal AMOUNT;
+
+        public string METHOD;
+
+        public PaymentExport()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PaymentExport"/> class.
+        /// </summary>
+        /// <param name="p">
+        /// The <see cref="Payment"/> to export
+        /// </param>
+        public PaymentExport(Payment p)
+        {
+            this.BILL_ID = p.BillId.ToString();
+            this.TIME = $"{p.Date.ToUniversalTime():yyyy-MM-ddTHH:mm:ssZ}";
+            this.AMOUNT = p.Bill.Amount;
+            this.METHOD = p.Method;
+        }
+    }
+}
---
+++ EzPay.IO/ExportWrappers/SettlementExport.cs
@@ -0,0 +1,62 @@
+﻿namespace EzPay.IO.ExportWrapper
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+
+    using EzPay.IO.Interfaces;
+    using EzPay.Model.Entities;
+
+    using FileHelpers;
+
+    /// <summary>
+    /// Wrapper class for exporting CSV records of <see cref="Settlement"/> entities
+    /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Required formatting for export, avoiding data annotation.")]
+    [SuppressMessage("ReSharper", "StyleCop.SA1600", Justification = "Fields are documented in corresponding classes")]
+    [SuppressMessage("ReSharper", "StyleCop.SA1401", Justification = "Entity is meant for record parsing, access modifier is Public for FileHelpers interop on fields")]
+    [DelimitedRecord(";")]
+    public class SettlementExport : IEntityRecord
+    {
+        public string VAT;
+
+        public string TIME;
+
+        public string BILLS;
+
+        [FieldConverter(ConverterKind.Decimal, ",")]
+        public decimal DOWNPAYMENT;
+
+        public int INSTALLMENTS;
+
+        [FieldConverter(ConverterKind.Decimal, ",")]
+        public decimal INTEREST;
+
+        public SettlementExport()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SettlementExport"/> class.
+        /// </summary>
+        /// <param name="s">
+        /// The <see cref="Settlement"/> to export
+        /// </param>
+        public SettlementExport(Settlement s)
+        {
+            var sb = new StringBuilder();
+            this.VAT = s.Bills.First().Citizen.Id.ToString();
+            this.TIME = s.Date.ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ");
+            foreach (var bill in s.Bills)
+            {
+                sb.Append($"{{{bill.Id}}}");
+            }
+
+            this.BILLS = sb.ToString();
+            this.DOWNPAYMENT = s.Type.Downpayment;
+            this.INSTALLMENTS = s.Installments;
+            this.INTEREST = s.Type.Interest;
+        }
+    }
+}
